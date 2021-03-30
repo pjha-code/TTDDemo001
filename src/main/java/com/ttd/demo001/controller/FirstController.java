456d22b8d9ea5119cd545b89359978f658138a94
pjha-code <pjhactr@gmail.com>
2021-03-30 15:01:50 +0530

    attempt 3 to run

--- src/main/java/com/ttd/demo001/controller/FirstController.java
+++ src/main/java/com/ttd/demo001/controller/FirstController.java
@@ -3,7 +3,7 @@ package com.ttd.demo001.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class FirstController {
     
     @GetMapping("/get")
